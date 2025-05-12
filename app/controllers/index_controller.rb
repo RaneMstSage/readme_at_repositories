f6965ad38b13343904ff74f91b12e6d1e8bf4d47
Erik-B. Ernst <erik.ernst@sic.software>
2025-05-12 16:09:36 +0200

    Don't call missing "unloadable"

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,8 @@
 class IndexController < ApplicationController
-  unloadable
+
+  if Redmine::VERSION::MAJOR < 4 || (Redmine::VERSION::MAJOR == 4 && Redmine::VERSION::MINOR < 1)
+    unloadable
+  end
 
   def index
     @project = Project.find(params[:project_id])
